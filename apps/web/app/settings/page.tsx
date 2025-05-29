410f2cb5752ac490dfe5f6aa9359de5a073bc403
Kemal <k.kemal.1989@gmail.com>
2025-05-29 06:05:19 +0200

    Implemented dummy settings page.

---
+++ apps/web/app/settings/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Ideas</h1>
+      <ul>
+        <li>Change Language</li>
+        <li>Change Theme</li>
+      </ul>
+    </div>
+  );
+}

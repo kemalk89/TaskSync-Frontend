56077e72678d23ba61114f5289ddc60da5b9e3d4
Kemal <k.kemal.1989@gmail.com>
2025-05-31 06:53:37 +0200

    Tests

--- packages/jest-config/index.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
-  },
-};
--- packages/ui-components/jest.config.js
+++ packages/ui-components/jest.config.js
@@ -1,6 +1,12 @@
 // Just importing the config from jsconfig workspace and exporting it
-import sharedConfig from "@app/jest-config";
 const config = {
-  ...sharedConfig,
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js",
+  },
 };
 export default config;

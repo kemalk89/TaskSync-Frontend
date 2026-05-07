2a9b6d093bcbbdaa5fc55c22e71eac1510c09b61
Kemal <k.kemal.1989@gmail.com>
2026-05-07 09:40:12 +0200

    Fix build issue

--- apps/web/app/user/login/page.tsx
+++ apps/web/app/user/login/page.tsx
@@ -1,5 +1,10 @@
 import { LoginPage } from "@app/ui-components";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <LoginPage />;
+  return (
+    <Suspense>
+      <LoginPage />
+    </Suspense>
+  );
 }

800eb8a0434b5484689cec42b5398d4c7d1938a0
Kemal <k.kemal.1989@gmail.com>
2026-09-21 10:49:33 +0200

    Polishing

--- packages/ui-components/src/features/user/signup/use-signup.ts
+++ packages/ui-components/src/features/user/signup/use-signup.ts
@@ -19,10 +19,17 @@ export const useSignup = () => {
     },
     onSuccess: (response) => {
       if (!response.ok) {
-        newToast({
-          type: "error",
-          msg: "Ungültige E-Mail-Adresse oder Passwort.",
-        });
+        if (response.status === 500) {
+          newToast({
+            type: "error",
+            msg: "Ein technischer Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.",
+          });
+        } else {
+          newToast({
+            type: "error",
+            msg: "Ungültige E-Mail-Adresse oder Passwort.",
+          });
+        }
       } else {
         newToast({
           type: "success",
